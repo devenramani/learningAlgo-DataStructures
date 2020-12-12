8e87362390c6b9cf3af2ba5e943de36369f5794e
Deven Ramani <30461878+devenramani@users.noreply.github.com>
2020-12-13 00:22:51 +0530

    creating structure

---
+++ Project/HotelManagement/src/ManagementSystem.java
@@ -0,0 +1,88 @@
+
+public class ManagementSystem implements IManagementSystem {
+
+	@Override
+	public int addDoubleRoom(String wing, int roomNumber, String status) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addFamilyRoom(String wing, int roomNumber, String status) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addClient(String name, String emailAddress) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printRooms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printClients() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int checkInDoubleRoom(int client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int checkInFamilyRoom(int client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean checkOutRoom(int client) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Vector searchAvailableRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printAvailableRooms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printOccupiedRooms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addWing(String wingName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void connectWings(String wing1, String wing2, double distance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void organizeCleaning() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
